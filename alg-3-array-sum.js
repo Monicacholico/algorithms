248b210368845a0d6ddadfda4ad12276d2af1851
Monicacholico <monicachol@hotmail>
2020-09-25 16:34:35 -0500

    test

--- alg-3-array-sum.js
+++ alg-3-array-sum.js
@@ -19,26 +19,4 @@ function sumUp(numbers) {
 console.log(sumUp(arra)); //8
 
 
-const array = [1, 2, 3]
-
-// const total = array.reduce( (preVal, currVal) => preVal + currVal)
-
-
-// console.log(total);
-
-function sumUp(numbers) {
-    let sum = 0; // 1
-
-    for (let i = 0; i < array.length; i++) {
-        sum += numbers[i];
-    }
-    return sum; // 1
-}
-
-//Linear Time Complexity  => O(n)
-
-console.log(sumUp(arra)); //8
-
-
-
 
