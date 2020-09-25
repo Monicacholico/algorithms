ee31be7a0edee8dee1537e9e948e7a96f7fb58c0
Monicacholico <monicachol@hotmail>
2020-09-25 16:26:18 -0500

    chapter 551

--- alg-1-minim.js
+++ alg-1-minim.js
@@ -1,21 +1,22 @@
 const numbers = [1,5,1];
 
-function findMin (numbers) {
-    if(!numbers.length) {
+function findMin (numbers) { // [3,1,2]
+    if(!numbers.length) { // 1 execution
         throw new Error ('Should not be an empty array!');
     }
 
-    let currentMinimum = numbers[0];
+    let currentMinimum = numbers[0]; // 1 execution
 
-    for (let i = 0; i < numbers.length; i++) {
-        if (numbers[i] < currentMinimum) {
-            currentMinimum = numbers[i];
+    for (let i = 0; i < numbers.length; i++) { // 1 execution
+        if (numbers[i] < currentMinimum) { // 2 executions
+            currentMinimum = numbers[i]; //1 execution
         }
     }
 
-    return currentMinimum;
-    
+    return currentMinimum; // 1 execution
 }
+
+// T = n => Linear Time Complexity => O(n)
 
 
 function getMin2(numbers) {
@@ -26,11 +27,12 @@ function getMin2(numbers) {
    
 
     for (let i = 0; i < numbers.lenght; i++) {
-        let outerElement = numbers[i];
+        let outerElement = numbers[i]; // n times
         for ( let j = i + 1; j < numbers.length; j++) {
-            let innerElement = numbers[j];
-            if (outerElement > innerElement) {
+            let innerElement = numbers[j]; // n times
 
+            if (outerElement > innerElement) {
+                //swap
                 numbers[i] = innerElement;
                 numbers[j] = outerElement; // [1, 3, 2]
 
@@ -41,10 +43,10 @@ function getMin2(numbers) {
             }
         }
     }
-
     return numbers[0];
-
 }
+
+// Quadratic Time Complexisty => n * n => O(n^2)
 
 
 const min = getMin2(numbers);
---
+++ alg-3-array-sum.js
@@ -0,0 +1,20 @@
+const array = [1, 2, 3]
+
+// const total = array.reduce( (preVal, currVal) => preVal + currVal)
+
+
+// console.log(total);
+
+function sumUp(numbers) {
+    let sum = 0; // 1
+
+    for (let i = 0; i < array.length; i++) {
+        sum += numbers[i];
+    }
+    return sum; // 1
+}
+
+//Linear Time Complexity  => O(n)
+
+console.log(sumUp(arra)); //8
+
